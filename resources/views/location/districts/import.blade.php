f5aacbdf4d170c43b3d188bab03a372552e45f19
viparvez <viparvez@gmail.com>
2019-11-11 03:09:39 +0600

    Minor Update

--- resources/views/location/districts/import.blade.php
+++ resources/views/location/districts/import.blade.php
@@ -64,6 +64,11 @@
 
         </div>
         <!-- /.col -->
+
+        <div class="row col-12">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="{{asset('/samples/district.csv')}}" download>Download Sample Template</a>
+        </div>
+        <!--ColEnd-->
       </div>
       <!-- /.row -->
     </section>
